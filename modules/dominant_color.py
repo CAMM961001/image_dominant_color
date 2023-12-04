9afa82b4056745db877a2261b259d88b10e7687b
CAMM961001 <miguel.castama@gmail.com>
2023-12-04 15:33:02 -0600

    Function to load url image

---
+++ modules/dominant_color.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import easygui
+
+from  urllib.request import urlopen
+
+
+def load_image(url:str):
+    with urlopen(url=url) as url:
+        with open('temp.jpg', 'wb') as file:
+            file.write(url.read())
+
+
+if __name__ == '__main__':
+    print('Hola mundo')
