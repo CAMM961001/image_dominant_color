6518d3ab260f986c5d6d0da2fd727c67cbd6656f
CAMM961001 <miguel.castama@gmail.com>
2023-12-04 15:58:50 -0600

    Delete unused function

--- modules/dominant_color.py
+++ modules/dominant_color.py
@@ -8,15 +8,5 @@ def load_image(url:str):
         with open('original.jpg', 'wb') as file:
             file.write(url.read())
 
-def color_count(img_path:str):
-    img = Image.open(img_path)
-    reduced = img.convert(mode='P', palette=Image.WEB)
-    palette = reduced.getpalette()
-    palette = [palette[3*n: 3*n + 3] for n in range(int(len(palette) / 3))]
-    color_count = np.array([(n, palette[m]) for n, m in reduced.getcolors()], dtype=object)
-
-    return color_count
-
-
 if __name__ == '__main__':
     print('Hola mundo')
