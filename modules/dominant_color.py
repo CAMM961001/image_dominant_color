a252989ea11152a17dc6b9e162df845d8810688b
CAMM961001 <miguel.castama@gmail.com>
2023-12-04 19:01:43 -0600

    Remove unused function

--- modules/dominant_color.py
+++ modules/dominant_color.py
@@ -1,3 +1,4 @@
+import webcolors
 import numpy as np
 from PIL import Image
 from  urllib.request import urlopen
@@ -7,6 +8,18 @@ def load_image(url:str):
     with urlopen(url=url) as url:
         with open('original.jpg', 'wb') as file:
             file.write(url.read())
+
+
+def closest_color(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
 
 if __name__ == '__main__':
     print('Hola mundo')
